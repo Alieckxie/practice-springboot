c397d89610f8aa03739313ab539ac17c184b296f
Alieckxie <Alieckxie@CopyLeft-PC>
2018-02-25 22:39:10 +0800

    初始化提交

---
+++ src/main/java/com/alieckxie/practice/springboot/PracticeSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.alieckxie.practice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alieckxie/practice/springboot/PracticeSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alieckxie.practice.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticeSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
